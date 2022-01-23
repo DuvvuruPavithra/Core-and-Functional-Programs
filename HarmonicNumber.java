16f8c197ebe55b1ad2d44144fec680c4ad38c5d5
DuvvuruPavithra <duvvurupavithra28@gmail.com>
2022-01-23 23:22:40 +0530

    Added: Adding the Harmonic Number uc4 is added in core Programs successfully

---
+++ HarmonicNumber.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class HarmonicNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.print("Enter any number : ");
+	        Scanner sc = new Scanner(System.in);
+	        int num = sc.nextInt();
+	        System.out.println("The Harmonic Number is : ");
+	        double result = 0;
+	        while(num > 0)
+	          {
+	               result = result + (double) 1 / num;
+	               num--;
+	               System.out.println(result +"  ");
+	          }
+	        System.out.println("");
+	        System.out.println("Output of Harmonic Series is "+result);
+
+	}
+
+}
